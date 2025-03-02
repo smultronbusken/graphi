f82152cb5ba58aae81fa47dde5939b71c62e2ee1
smultron <fredholmisak@outlook.com>
2025-03-02 16:19:26 +0100

    change styling for inactive nodes and edges

--- src/pixi/components/line.ts
+++ src/pixi/components/line.ts
@@ -46,7 +46,7 @@ export class Line {
             case "inactive":
                 this.line.filters = [this.grayscaleFilter];
                 this.grayscaleFilter.desaturate();
-                this.line.stroke({ width: 3, color: 0xaaaaaa, alpha: 0.5 });
+                this.line.stroke({ width: 1, color: 0xaaaaaa, alpha: 0.5 });
                 break;
         }
     }
