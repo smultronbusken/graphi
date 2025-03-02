8f2d8e6c23322a845a79b792f7e1f4562cf6ebbe
smultron <fredholmisak@outlook.com>
2025-03-02 16:16:06 +0100

    add shortest path search

--- src/graph/path.ts
+++ src/graph/path.ts
@@ -1,9 +1,16 @@
 import { GraphiGraph } from "./types"
-import {allSimpleEdgePaths} from 'graphology-simple-path';
-
+import { allSimpleEdgePaths } from 'graphology-simple-path';
+import { bidirectional } from 'graphology-shortest-path';
 
 export const getPaths = (source: string, target: string, graph: GraphiGraph) => {
     // Ehmmmm this is not directed search
-    const paths = allSimpleEdgePaths(graph, source, target) 
+    const paths = allSimpleEdgePaths(graph, source, target)
     return paths
+}
+
+
+export const shortestPath = (source: string, target: string, graph: GraphiGraph) => {
+    const path = bidirectional(graph, source, target)
+    console.log()
+    return path
 }
